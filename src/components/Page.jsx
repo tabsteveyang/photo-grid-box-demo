f2c7c1bb7e1e35cadc0c5da0b15966b32325fd4e
tabsteveyang <tabsteveyang@gmail.com>
2021-10-03 12:04:10 +0800

    REFACTOR: update to version 2

---
+++ src/components/Page.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import PhotoGridBox from 'photo-grid-box';
+
+const Page = () => {
+  const [imgs, setImgs] = useState([{
+      src: "https://c1.staticflickr.com/1/699/22812601591_12ca1ee7cf_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/1/573/22409354059_ba46782c8f_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/6/5704/22410267477_303a090dcd_m.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/1/683/22207558073_8ecdb7abc4_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/6/5735/22872942450_7885235293_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/6/5772/22601600994_083f34661c_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/1/731/23230259025_673793e478_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/3/2937/14472303464_1890a1c27b_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4665/38432665950_12d8d33002_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4649/38432665780_71b232cc13_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4704/38432663720_c233066c15_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4677/40243327101_965347ed63_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4696/38432663200_459cbab4dd_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4650/40243326751_cb7845848f_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4709/38432662890_06b0ac1600_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4616/38432662220_4874c887f1_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4719/38432661400_f53017f598_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4753/38432660740_78d6d34d89_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4654/39345481745_1be0a0098c_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4704/38432657930_c96c750753_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4754/38432659650_9d6e5876a1_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4609/39345480685_62fafe15e2_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4664/38432658670_ff133e5369_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4702/39533019874_875e198031_n.jpg"
+    },
+    {
+      src: "https://c1.staticflickr.com/5/4703/25372643047_302849c0d0_n.jpg"
+    },
+  ])
+
+  const [rowGap, setRowGap] = useState(3)
+  const [colGap, setColGap] = useState(3)
+  const [showUnCompleteRow, setShowUnCompleteRow] = useState(true)
+
+  const imgOnClick = (e, imgConfig) => {
+    console.log('img clicked!', e, imgConfig)
+  }
+  const panelHTMLSetter = (imgConfig) => {
+    let result = null
+    if (imgConfig && imgConfig.payload && imgConfig.payload.title) {
+      result = <div className="photo-block__panel__title"> { imgConfig.payload.title } </div>
+    }
+    return result
+  }
+
+  return <div className='page'>
+    {
+      <PhotoGridBox
+        imgs = {imgs} // set the pictures to show
+        rowGap = {rowGap} // set the height between each row (optional)
+        colGap = {colGap} // set the width between each block (optional)
+        imgOnClick = {imgOnClick} // the onClick event handler for each block (optional)
+        panelHTMLSetter = {panelHTMLSetter} // the function that returns a JSX for adding the children on the panel (optional)
+        showUnCompleteRow = {showUnCompleteRow} // In default, the PhotoGridBox will hide the last row if the last row is not complete; to make it looks more natural when loading pictures chunk by chunk. When there is no more picture to load, or for any reason, you can set the prop to true cancel the feature. (optional)
+      />
+    } </div>
+}
+
+export default Page
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require("path"); //a build in module in nodeJS
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-//argument env's value is set by --env value.
 module.exports = (env) => {
     const isProduction = env === "production";
 
     return {
-        //input source file
-        entry: path.join(__dirname, "src", "app.js"), //app.js
-        //output bundle file
+        entry: path.join(__dirname, "src", "app.js"),
         output: {
             path: path.join(__dirname, "public", "dist"),
             filename: 'bundle.js'
         },
-        //adding babel configuration
         module: {
             rules: [{
-                loader: 'babel-loader',    //specify a loader
-                test: /\.js$/,             //point out the file that loader has to compile
-                exclude: /node_modules/    //if the file is under this path, then ignore it
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
             },{
                 test: /\.s?css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        "css-loader",
-                        "sass-loader",
-                    ]
-                })
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
             }]
         },
         plugins: [
-            new ExtractTextPlugin("styles.css"),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            }),
+            new HtmlWebpackPlugin({
+                minify: false,
+                template: '/src/template/index.html',
+                filename: 'index.html'
+              })
         ],
-        //devtool: 'cheap-module-eval-source-map',
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', //use another kind of source map for production.
-        //develop Server
-        devServer: {
-            contentBase: path.join(__dirname, "public"),
-            disableHostCheck: true,
-            historyApiFallback: true,
-            publicPath: "/dist/"
-        }
+        devtool: isProduction ? false : 'source-map'
     };
 };
